e286b490cb5598f6078fcc3f30abbcc7db47ae4f
vintorez10 <vintorez10@mail.ru>
2020-06-21 20:32:45 +0300

    Update client.cpp

--- ClientSide/client.cpp
+++ ClientSide/client.cpp
@@ -17,7 +17,7 @@ void Client::send_request(const std::string& request)
 std::string Client::get_responce()
 {
     //The response message sent by the server may differ in length, but must end as follows:
-    // '\n ' so we use this character as a separator when calling the function
+    // '\n' so we use this character as a separator when calling the function
     boost::asio::read_until(m_client_socket, m_buffer, '\n');
 
     //putting the received data from the buffer into the stream
